af04f78dcfa8de034d02d650bfd17ba96f87b11e
konlawat <biu0862745993@gmail.com>
2024-11-21 17:15:08 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/lab1_029/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab1_029
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("652021029"),
+        backgroundColor: Colors.amber,
+      ),
+    );
+  }
+}
